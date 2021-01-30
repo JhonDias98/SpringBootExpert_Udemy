d792b240b8685ee2f5074a6951bcb3a44b1de831
Jonathan Dias <jonathan.dias.p2017@gmail.com>
2021-01-30 20:20:01 -0300

    API

---
+++ src/main/java/io/github/JhonDias98/domain/repository/ItemsPedido.java
@@ -0,0 +1,7 @@
+package io.github.JhonDias98.domain.repository;
+
+import io.github.JhonDias98.domain.entity.ItemPedido;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemsPedido extends JpaRepository<ItemPedido, Integer> {
+}
---
+++ src/main/java/io/github/JhonDias98/domain/repository/Produtos.java
@@ -0,0 +1,7 @@
+package io.github.JhonDias98.domain.repository;
+
+import io.github.JhonDias98.domain.entity.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface Produtos extends JpaRepository<Produto,Integer> {
+}
